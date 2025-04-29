054c7f1f5de71b25bf9982481a3fcfd9e1cb3d45
Cédric BOUCHER <cedric.boucher1990@gmail.com>
2025-04-29 10:15:19 +0200

    feat : Ajout de la fonction handleVariantClick pour afficher les variants.

--- src/components/ProductId.jsx
+++ src/components/ProductId.jsx
@@ -13,7 +13,6 @@ function ProductId() {
       .then((data) => {
         console.log(data);
         setProductItem(data);
-        console.log(data.product.name);
       })
       .catch((error) => console.error('Error fetching productItem details:', error));
   }, [id]);
@@ -22,36 +21,56 @@ function ProductId() {
     return <div>Loading...</div>;
   }
   console.log("teste vue : ", productItem);
-  console.log("productVariants volume : ", productItem.product.product_variants[0].volume);
+
+
+  const handleVariantClick = (productVariant) => {
+    for (let i = 0; i < productItem.product.product_variants.length; i++) {
+      const variantId = productItem.product.product_variants[i].id;
+      console.log("le variantId : ", variantId);
+
+      if (productVariant.id === variantId) {
+        console.log("le product Variant : ", productVariant.id);
+        document.getElementById(productVariant.id).style.display = 'block';
+      } else {
+        document.getElementById(productItem.product.product_variants[i].id).style.display = 'none';
+      }
+    }
+  };
 
   return (
     <div>
       <img src={productItem.product.image} alt={productItem.product.name} />
-      <h1>{productItem.product.name}</h1>
+      <h1>{productItem.product.name} <span>Une bière d'une force à {productItem.product.alcohol_degree} % d'Alc</span></h1>
 
-      {productItem.product.product_variants.map((productItem) => (
-        <div>
-          <span>{productItem.volume} - </span>
-          <span>{productItem.price_without_tax / 100} € </span>
-          <span>{productItem.available ? '' : 'La soif était trop grande !'}</span>
+      {productItem.product.product_variants.map((productVariant) => (
+        <div key={productVariant.id}>
 
+          <div id={productVariant.id} style={{ display: 'block' }}>
+            <span>{productVariant.price_without_tax / 100} € - </span>
+            <span>
+              {productVariant.available 
+                ? `Stock ${productVariant.stock_quantity}` 
+                : 'La soif était trop grande !'}
+            </span>
+          </div>
+
+          <button onClick={() => handleVariantClick(productVariant)}>
+            {productVariant.volume}
+          </button>
         </div>
-      ) )}
-
+      ))}
 
       <h2>Description :</h2>
       <p>{productItem.product.description}</p>
       
-      
-      {productItem.brands.map((productItem) => (
-        <div>
-          <h3>{productItem.name} </h3>
-          <p>{productItem.description}</p>
+      {productItem.brands.map((brand) => (
+        <div key={brand.id}>
+          <h3> <img src={brand.logo} alt={brand.name} /> {brand.name} </h3>
+          <p>{brand.description}</p>
         </div>
-      ) )}
-  </div>
-
-    )
+      ))}
+    </div>
+  );
   };
 
 export default ProductId;
