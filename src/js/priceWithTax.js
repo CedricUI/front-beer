cecefa170605f670fdf39d7c1a642d0f4f3d408b
Cédric BOUCHER <cedric.boucher1990@gmail.com>
2025-05-20 10:01:45 +0200

    feat : ajout de la méthode priceWithTax dans la page  ProductId

--- src/js/priceWithTax.js
+++ src/js/priceWithTax.js
@@ -1,9 +1,7 @@
 function priceWithTax(price_without_tax, tax_amount) {
     const priceWithTax = (price_without_tax + ((price_without_tax * tax_amount) / 100))/100;
     const roundedPrice = priceWithTax.toFixed(2);
-    // console.log('priceWithTax: ' + priceWithTax + ' roundedPrice: ' + roundedPrice);
     const formattedPrice = roundedPrice.replace('.', ',');
-    // console.log('formattedPrice: ' + formattedPrice + ' roundedPrice: ' + roundedPrice + ' priceWithTax: ' + priceWithTax);
     return formattedPrice;
 }
 
